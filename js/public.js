ac3d12a3977be4969df5803610fb6b5107fd67fe
luzhilin1 <2275329515@qq.com>
2018-09-04 15:35:30 +0800

    第一次提交

---
+++ js/public.js
@@ -0,0 +1,9 @@
+function rem(){
+			var html = document.documentElement;
+			var w = html.offsetWidth
+			html.style.fontSize = 100/750*w + "px";
+			
+			}
+			rem();
+			window.onresize = rem
+			
